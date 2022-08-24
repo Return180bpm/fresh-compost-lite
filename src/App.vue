bba2540d2a4c5df571caff858b253e4f6adbd6cc
Tom Szwaja <th.szwaja@gmail.com>
2022-08-24 11:47:06 +0200

    refactor(vuetify): put <routerview> inside <v-main
    as suggested here
    https://next.vuetifyjs.com/en/components/application/

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <v-app>
-    <router-view />
+    <v-main font-sans p="x-4 y-10" text="center gray-700 dark:gray-200" class="container mx-auto flex flex-col items-center justify-center">
+      <router-view />
+    </v-main>
   </v-app>
 </template>
 
