264dadc0680135030e0fb55219244d2fa374575a
Tom Szwaja <th.szwaja@gmail.com>
2022-08-26 12:21:53 +0200

    fix(css): remove clashing text color

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <main font-sans p="x-4 y-10" text="center gray-700 dark:gray-200" class="w-full container mx-auto flex flex-col items-center justify-center">
+  <main font-sans p="x-4 y-10" text="center dark:gray-200" class="w-full container mx-auto flex flex-col items-center justify-center">
     <router-view />
   </main>
 </template>
